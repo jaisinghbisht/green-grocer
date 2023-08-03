38c4f5c6f9bf82f6cc776f25af255e9de13ad20e
Ayush20021989 <100331783+Ayush20021989@users.noreply.github.com>
2023-08-03 14:42:48 +0530

    Update App.js
    footer and header added, cart page working

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import Home from "./components/Home";
 import About from "./pages/About";
 import Shopping from "./pages/Shopping";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Cart from "./pages/Cart";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/shopping" element={<Shopping />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
