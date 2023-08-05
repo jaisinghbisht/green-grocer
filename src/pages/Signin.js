1a329d452943a8e5cf33ab20aed332c06ba03baa
Jai Singh Bisht <jaisinghbisht.lko@gmail.com>
2023-08-05 11:41:39 +0530

    Tried fixing some things

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,50 +1,54 @@
-import React from 'react'
-import "../styles/s.css"
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../styles/s.css";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
   return (
     <div>
       <body>
-
-<div className="wrapper">
-    <form action="/login" method="POST" className="form-box">
-        <div className="login-container" id="login">
-            <div className="top">
-                <span>Don't have an account? <Link to="/Signup">Register
-                </Link></span>
+        <div className="wrapper">
+          <form action="/login" method="POST" className="form-box">
+            <div className="login-container" id="login">
+              <div className="top">
+                <span>
+                  Don't have an account? <Link to="/Signup">Register</Link>
+                </span>
                 <header>Log In</header>
-            </div>
-            <div className="input-box">
-                <input type="text" className="input-field" placeholder="Username" name="username"/>
+              </div>
+              <div className="input-box">
+                <input
+                  type="text"
+                  className="input-field"
+                  placeholder="Username"
+                  name="username"
+                />
                 <i className="bx bx-user"></i>
-            </div>
-            <div className="input-box">
-                <input type="password" className="input-field" placeholder="Password" name="password"/>
+              </div>
+              <div className="input-box">
+                <input
+                  type="password"
+                  className="input-field"
+                  placeholder="Password"
+                  name="password"
+                />
                 <i className="bx bx-lock-alt"></i>
-            </div>
-            <div className="input-box">
-                <input type="submit" className="submit" value="Sign In"/>
-            </div>
-            <div className="two-col">
+              </div>
+              <div className="input-box">
+                <input type="submit" className="submit" value="Sign In" />
+              </div>
+              <div className="two-col">
                 <div className="one">
-                    <input type="checkbox" id="login-check"/>
-                    <label for="rlogin-check">Remember Me</label>
+                  <input type="checkbox" id="login-check" />
+                  <label for="rlogin-check">Remember Me</label>
                 </div>
-                <div className="two">
-                   
-                </div>
+                <div className="two"></div>
+              </div>
             </div>
+          </form>
         </div>
-
-       
-    </form>
-</div>
-
-
-</body>
+      </body>
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import "../styles/s.css"
-import { Link } from 'react-router-dom'
-import axios from 'axios';
+import React, { useState } from "react";
+import "../styles/s.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
 const Signup = () => {
-   /*  const [fname, setField1] = useState('');
+  /*  const [fname, setField1] = useState('');
     const [lname, setField2] = useState('');
     const [username, setField3] = useState('');
     const [email, setField4] = useState('');
@@ -29,70 +29,95 @@ const Signup = () => {
     }; */
 
   return (
-  <div>
+    <div>
       <body>
         <div className="wrapper">
-            <form action="POST" /* onSubmit={handleSubmit} */ className="form-box">
-                <div className="register-container" id="register">
-                    <div className="top">
-                        <span>Have an account? <Link to="/Signin">Login
-                </Link></span>
-                        <header>Sign up</header>
-                    </div>
-                    <div className="two-forms">
-                        <div className="input-box">
-                            <input type="text" className="input-field" placeholder="First Name" name="fname"  /* onChange={(e) => setField1(e.target.value)} *//>
-                            <i className="bx bx-user"></i>
-                        </div>
-                        <div className="input-box">
-                            <input type="text" className="input-field" placeholder="Last Name" name="lname" /* onChange={(e) => setField2(e.target.value)} *//>
-                            <i className="bx bx-user"></i>
-                        </div>
-                    </div>
-                    
-                    <div className="input-box">
-                        <input type="text" className="input-field" placeholder="Username" name="username"/*  onChange={(e) => setField3(e.target.value)} *//>
-                        <i className="bx bx-user"></i>
-                    </div>
-                    
-                    <div className="input-box">
-                        <input type="email" className="input-field" placeholder="Email" name="email" /* onChange={(e) => setField4(e.target.value)} *//>
-                        <i className="bx bx-envelope"></i>
-                    </div>
-
-                    <div className="input-box">
-                        <input type="password" className="input-field" placeholder="Password" name="password"/* onChange={(e) => setField5(e.target.value)} *//>
-                        <i className="bx bx-lock-alt"></i>
-                    </div>
-                   
-
-                    <div className="input-box">
-                        <input type="submit" name="signup" className="submit" value="Register" />
-                    </div>
-                  
-
-                    <div className="two-col">
-                        <div className="one">
-                            <input type="checkbox" id="register-check"/>
-                            <label for="register-check">Remember Me</label>
-                        </div>
-                        <div className="two">
-                            <label>
-                                
-                            </label>
-                        </div>
-                    </div>
+          <form
+            action="POST"
+            /* onSubmit={handleSubmit} */ className="form-box"
+          >
+            <div className="register-container" id="register">
+              <div className="top">
+                <span>
+                  Have an account? <Link to="/Signin">Login</Link>
+                </span>
+                <header>Sign up</header>
+              </div>
+              <div className="two-forms">
+                <div className="input-box">
+                  <input
+                    type="text"
+                    className="input-field"
+                    placeholder="First Name"
+                    name="fname" /* onChange={(e) => setField1(e.target.value)} */
+                  />
+                  <i className="bx bx-user"></i>
                 </div>
+                <div className="input-box">
+                  <input
+                    type="text"
+                    className="input-field"
+                    placeholder="Last Name"
+                    name="lname" /* onChange={(e) => setField2(e.target.value)} */
+                  />
+                  <i className="bx bx-user"></i>
+                </div>
+              </div>
 
-              
-            </form>
+              <div className="input-box">
+                <input
+                  type="text"
+                  className="input-field"
+                  placeholder="Username"
+                  name="username" /*  onChange={(e) => setField3(e.target.value)} */
+                />
+                <i className="bx bx-user"></i>
+              </div>
+
+              <div className="input-box">
+                <input
+                  type="email"
+                  className="input-field"
+                  placeholder="Email"
+                  name="email" /* onChange={(e) => setField4(e.target.value)} */
+                />
+                <i className="bx bx-envelope"></i>
+              </div>
+
+              <div className="input-box">
+                <input
+                  type="password"
+                  className="input-field"
+                  placeholder="Password"
+                  name="password" /* onChange={(e) => setField5(e.target.value)} */
+                />
+                <i className="bx bx-lock-alt"></i>
+              </div>
+
+              <div className="input-box">
+                <input
+                  type="submit"
+                  name="signup"
+                  className="submit"
+                  value="Register"
+                />
+              </div>
+
+              <div className="two-col">
+                <div className="one">
+                  <input type="checkbox" id="register-check" />
+                  <label for="register-check">Remember Me</label>
+                </div>
+                <div className="two">
+                  <label></label>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
-
-
       </body>
+    </div>
+  );
+};
 
-  </div >
-  )
-}
-
-export default Signup
+export default Signup;
