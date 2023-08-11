956d5de6df20e5597e8e66a249d5af64992a35d0
Rajat Ghadwal <98544325+Rajat-Ghadwal@users.noreply.github.com>
2023-08-11 13:03:49 +0530

    Update contactus.js

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import '../styles/contactus.css'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import styled from "styled-components";
 
 function ContactUs() {
   useEffect(() => {
@@ -18,37 +19,89 @@ function ContactUs() {
   function loadFooter() {
     load('footer.html', 'footer');
   }
-
+  const Contact = () => {
+    const Wrapper = styled.section`
+      padding: 9rem 0 5rem 0;
+  
+      .container {
+        margin-top: 6rem;
+        text-align: center;
+  
+        .contact-form {
+          max-width: 50rem;
+          margin: auto;
+  
+          .contact-inputs {
+            display: flex;
+            flex-direction: column;
+            gap: 3rem;
+  
+            input[type="submit"] {
+              cursor: pointer;
+              transition: all 0.2s;
+  
+              &:hover {
+                background-color: ${({ theme }) => theme.colors.white};
+                border: 1px solid ${({ theme }) => theme.colors.btn};
+                color: ${({ theme }) => theme.colors.btn};
+                transform: scale(0.9);
+              }
+            }
+          }
+        }
+      }
+      `;
+    }
   return (
-    <div>
-      <div className="container2">
-        <section>
-          <h1 align="center">Contact Us</h1>
-          <p align="center">We'd love to hear from you! If you have any questions, feedback, or inquiries, please feel free to get in touch with us using the contact form below or through the provided contact details.</p>
-        </section>
-      </div>
-      <div className="container12">
-        <form>
-          <h1>Opinion Form</h1>
-          <input type="text" id="firstName" placeholder="First Name" required />
-          <input type="text" id="lastName" placeholder="Last Name" required />
-          <input type="email" id="email" placeholder="Email" required />
-          <input type="text" id="mobile" placeholder="Mobile" required />
-          <h4>Type Your Message Here...</h4>
-          <textarea required></textarea>
-          <input type="submit" value="Send" id="button" />
+    <Wrapper>
+    <h2 className="common-heading">Feel Free to Contact us</h2>
+
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15126.28620995241!2d73.92422475000001!3d18.59334505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c14df5c70e0d%3A0x2d19689e09e2fced!2sPhoenix%20Mall%20Washrooms!5e0!3m2!1sen!2sin!4v1658905192255!5m2!1sen!2sin"
+      width="100%"
+      height="450"
+      style={{ border: 0 }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+    <div className="container">
+      <div className="contact-form">
+        <form
+          action="https://formspree.io/f/xgedgjkr"
+          method="POST"
+          className="contact-inputs">
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            autoComplete="off"
+            required
+          />
+
+          <input
+            type="email"
+            name="Email"
+            placeholder="Email"
+            autoComplete="off"
+            required
+          />
+
+          <textarea
+            name="message"
+            cols="30"
+            rows="6"
+            autoComplete="off"
+            required></textarea>
+
+          <input type="submit" value="send" />
         </form>
       </div>
-      <section className="contact-us">
-        <div className="contact-col">
-          <section className="location">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7004.877868332033!2d77.1929019586914!3d28.61660407423769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce2a99b6f9fa7%3A0x83a25e55f0af1c82!2sRashtrapati%20Bhavan!5e0!3m2!1sen!2sin!4v1690561727916!5m2!1sen!2sin" width="600" height="450" style={{ border: '0' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </section>
-        </div>
-      </section>
-      <div id="footer"></div>
     </div>
-  );
-}
+  </Wrapper>
+);
+};
+
 
 export default ContactUs;
+
